6165fd300afedca154f2288f0aa7778b18db09cc
SanyamPunia <lewarends@gmail.com>
2022-03-23 17:30:21 +0530

    toggle issue v1

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider attribute="class" defaultTheme="dark">
       <Component {...pageProps} />
     </ThemeProvider>
   )
