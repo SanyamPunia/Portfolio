4783175f2dd4e173cf9f3f338aaceeb564ae0e67
SanyamPunia <lewarends@gmail.com>
2022-05-15 02:13:31 +0530

    key attr for map element 'stacks'

--- components/project/ProjectStack.js
+++ components/project/ProjectStack.js
@@ -1,10 +1,12 @@
 const ProjectStack = (props) => {
     const stacks = props.projectStack;
+    console.log(stacks);
 
     return (
         <>
             {stacks.map(stack => (
                 <div
+                    key={stack._key}
                     className={
                         `${stack.style === 'h1' && 'text-bgBlack dark:text-mainWhite text-3xl font-mulish font-extrabold mb-3'}
                          ${stack.listItem === 'bullet' && 'list-item-stack ml-10 font-mulish text-lg text-divider text-md dark:text-blogDescriptionGray'}`
