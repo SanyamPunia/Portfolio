22612975bcde0a0c2f00d542a4d1265854c50e20
SanyamPunia <lewarends@gmail.com>
2023-02-26 23:43:49 +0530

    add layout metadata (root) using openGraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,29 @@ import "app/globals.css";
 import type { Metadata } from "next/types";
 
 export const metadata: Metadata = {
-  title: "Sanyam | About",
-  description: "Sanyam's Personal Portfolio",
+  title: "Sanyam | Personal Portfolio",
+  description:
+    "Welcome to my peronsal portfolio where you'll get to know about my experiences of my professional life. I love building personal/pet projects & writing technical articles. I also make music as my side hobby.",
+  openGraph: {
+    title: "Sanyam | Personal Portfolio",
+    description:
+      "Welcome to my peronsal portfolio where you'll get to know about my experiences of my professional life. I love building personal/pet projects & writing technical articles. I also make music as my side hobby.",
+    url: "https://sanyam.xyz/",
+    siteName: "Sanyam's Portfolio",
+    images: [
+      {
+        url: "https://sanyam.xyz/assets/og-image.png",
+        width: "1920",
+        height: "1080",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({
