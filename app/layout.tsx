3e0ef7e76cdf0c649bbdf2c62176faec0540df09
SanyamPunia <lewarends@gmail.com>
2023-02-27 00:00:31 +0530

    max-image-preview for googlebot

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,13 @@ export const metadata: Metadata = {
   robots: {
     index: true,
     follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
   },
 };
 
